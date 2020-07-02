88795541963ad0cf40477af1e212b3797c3d5f75
Ahn <anhpnnd@gmail.com>
2020-07-02 22:44:49 +0200

    chore: add typing suggestion for jest.config.js and set target to es2019

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 // const { jsWithBabel: tsjPreset } = require('ts-jest/presets');
-
+/** @typedef {import('ts-jest')} */
+/** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   preset: 'ts-jest/presets/js-with-babel',
   globals: {
